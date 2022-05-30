716e8427a279d4f5e828f2758bbbad070ba6561c
Fulmych <105441072+Fulmych@users.noreply.github.com>
2022-05-30 12:56:47 +0200

    Exercise3

---
+++ Exercise3/src/application/Exercise3.java
@@ -0,0 +1,15 @@
+package application;
+public class Exercise3 {
+    public static void main(String[] arge){
+        int a = 66;
+        int b = 11;
+        int c = a + b;
+        int d = a * b;
+        int e = a / b;
+        int f = a - b;
+        System.out.println("The total of " + a + " + " + b + " = " + c + ".");
+        System.out.println("The total of " + a + " * " + b + " = " + d + ".");
+        System.out.println("The total of " + a + " / " + b + " = " + e + ".");
+        System.out.println("The total of " + a + " - " + b + " = " + f + ".");
+    }
+}
